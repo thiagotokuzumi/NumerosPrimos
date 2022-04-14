a3c22f2049330f0c79435f09d30bbe1bc084b81b
Thiago Tokuzumi <thiagotokuzumi@gmail.com>
2022-04-14 13:25:34 -0300

    Versão 1.0

---
+++ NumerosPrimos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace NumerosPrimos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um número que deseja verificar se é primo ou não, caso não seja," +
+                "retornará seu divisor maior que 1: ");
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            Console.Clear();
+
+            int resultado = PrimoOuNao(n);
+            Console.Write(resultado);
+        }
+
+        static int PrimoOuNao(long n)
+        {
+            int flag = 0, result = 1;
+            long divider = n / 2;
+
+            /* Number Prime Check */
+            for (int i = 2; i <= divider; i++)
+            {
+                if (n % i == 0)
+                {
+                    flag = 1;
+                }
+            }
+
+
+            /* not prime */
+            if (flag == 1)
+            {
+                /* Retornar divisor do número não primo > 1 */
+                if (n % 2 == 0)
+                {
+                    result = 2;
+                }
+                else
+                {
+                    result = 3;
+                }
+            }
+
+            return result;
+
+        }
+    }
+
+}
